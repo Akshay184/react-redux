c11a4e14e65334c54cb8a64fa37418e67d439f93
Akshay184 <akshayagarwal184@gmail.com>
2020-06-06 12:18:27 +0530

    added middlerware in redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './store/reducer';
 import { Provider } from "react-redux";
 
@@ -9,7 +9,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer);
+const logger = (store) => {
+    return (next) => {
+        return (action) => {
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action);
+            console.log('[Middleware] next state', store.getState());
+            return result;
+        }
+    }
+}
+
+const store = createStore(reducer, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store = {store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
